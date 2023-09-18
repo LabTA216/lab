5daa8d52e5685e869708927f83fb2bbd0c7f6e50
2211201215 <lwork0921@gmail.com>
2023-09-18 15:45:21 +0530

    1

--- DSALAb/lab5Q4.c
+++ DSALAb/lab5Q4.c
@@ -7,12 +7,14 @@ struct Node{
 };
 typedef struct Node dlist;
 
-dlist* inputdlist(dlist ** head,int n){
+dlist* inputList(dlist ** head,int n){
     (*head)->next=NULL;
-    dlist * p=* head;
+    (*head)->prev=NULL;
+    dlist * p=* head,*k=NULL;
     for (int i = 0; i < n; i++)
     {
         dlist * ptr=(dlist*)malloc(sizeof(dlist));
+        ptr->next=NULL;
         int x;
         scanf("%d",&x);
         p->data=x;
@@ -22,16 +24,22 @@ dlist* inputdlist(dlist ** head,int n){
         }
         p->next=ptr;
         p=p->next;
+        p->prev=k;
+        k=p;
     }
     return p;
 }
-
-
-
-void printdlist(dlist * head) {
+void printList(dlist * head) {
       while (head != NULL) {
       printf("%d ", head->data);
       head = head->next;
+   }
+   printf("\n");
+}
+void printListattail(dlist * head) {
+      while (head != NULL) {
+      printf("%d ", head->data);
+      head = head->prev;
    }
    printf("\n");
 }
@@ -48,17 +56,35 @@ dlist* convertd(dlist* head){
     return t;
 
 }
+
+void reversedlist(dlist ** head){
+      dlist * current = *head,*t,*l;
+      while (current != NULL) {
+            t=current->next;
+            current->next=current->prev;
+            l=current->prev;
+            current->prev=t;      
+            current=current->prev;
+
+      }
+      *head=l->prev;
+}
+
 int main(int argc, char const *argv[])
 {
-    dlist *tail,* head=(dlist*)malloc(sizeof(dlist)),*p,*k;
+    dlist *tail, * head=(dlist*)malloc(sizeof(dlist)),*p,*k;
      head->next=NULL;
      printf("Enter the number of elements ");
      int n;
      scanf("%d",&n);
-    tail=inputdlist(&head,n);
-    // printdlist(head);
+    tail=inputList(&head,n);
+    // printList(head);
     k=convertd(head);
-    // printdlist(k);
-    printdlist(k);
+    // printList(k);
+    printList(k);
+    // printListattail(tail);
+    reversedlist(&head);
+    printList(head);
+
     return 0;
 }
